d2420f891d5f158535ade43386de4cb9eaf58871
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-01-15 16:24:02 +0900

    etc

---
+++ asset/script.js
@@ -0,0 +1,93 @@
+// null = default
+const config = {
+    "loadType": "",
+    "spaceArg": [
+        {
+            name: "%SiteTitle%", content: ""
+        },
+    ],
+    // 変更を加えても意味なし
+    // "pathname": {
+    //     "post": "posts/%parameter%",
+    //     "paged": "%pagedNumber%",
+    //     "page": "%parameter%"
+    // },
+    "maxPosts": 9
+}
+
+/**
+ * main script for create website.
+ * @author "github.com/Nobody-Harun"
+ */
+
+/**
+ * Call Website Resources
+ */
+class Resource {
+    /**
+     * @returns {object}
+     */
+    constructor() {
+        return {
+            "posts": this.posts(),
+        }
+    }
+    /**
+     * return post data
+     * @returns {object | Array}
+     */
+    get posts() {
+        const res = fetch("/posts/data.jsonc");
+        const data = res.json();
+        return data;
+    }
+    /**
+     * return Website Widget Data
+     * @returns {object | Array}
+     */
+    get Widgets() {
+        let cache = document.documentElement.innerHTML;
+        config.spaceArg.forEach(data => {
+            cache = cache.replaceAll(data.name, data.content);
+        })
+        document.documentElement.innerHTML = cache;
+    }
+}
+
+/**
+ * Run App(WebSite Resources). if run this, Load & Write at in Element: "#container"
+ * 
+ */
+class App extends Resource {
+    constructor() {
+        // Start Setup
+        this.requestView();
+    }
+    requestView() {
+        this.InstantDB = super();
+        if (this.InstantDB == (null || undefined || "")) return console.error("couldn't requestView: null of this.InstantDB");
+
+        // post
+        if (location.pathname.startsWith("posts")) {
+            this.InstantDB.posts.forEach(data => {
+                if (data.parameter == location.pathname.split("/")[1]) {
+                }
+            })
+        } else if (typeof location.pathname == "number") {
+            this.InstantDB.posts.slice(9)
+        } else {
+            
+        }
+
+        // Rewrite SpaceArg
+        super.Widgets();
+    }
+}
+
+// on Startupped(DOMContentLoaded)
+window.addEventListener("DOMContentLoaded", () => {
+    const app = new App(config.loadType);
+
+    document.title = "%SiteTitle%";
+    app.requestView();
+})
