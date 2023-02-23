565ec5109b0dde66e5f4a846979cd31229c6953b
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-02-23 22:36:41 +0900

    Fix

--- runner.js
+++ runner.js
@@ -1,8 +1,41 @@
-const path = require('path');
-const fs = require('fs');
-const { marked } = require("./config/lib/md-reader.js");
+try {
+    const path = require('path');
+    const fs = require('fs');
+    const { marked } = require("./config/lib/md-reader.js");
 
-const config = JSON.parse(fs.readFileSync("./config/config.json", {encoding: 'utf-8'}));
+    const config = JSON.parse(fs.readFileSync("./config/config.json", {encoding: 'utf-8'}));
+
+    const template = fs.readFileSync(config.design.template, {encoding: 'utf-8'});
+
+    fs.mkdirSync("public");
+
+    fs.readdirSync("./pages", {withFileTypes: true})
+        .filter(dirent => dirent.isFile()).map(({name}) => name)
+        .filter(file => file.toLowerCase().endsWith(".md"))
+        .forEach(page => {
+            const name = page.split("/")[page.split("/").length - 1].substring(0, page.split("/")[page.split("/").length - 1].length - 3);
+            if (name == "articles") throw new Error("You must not use name: Articles");
+            const data = marked.markup(fs.readFileSync(page, {encoding: 'utf-8'}));
+
+            let cache = template;
+
+            [
+                ["title", `${data.meta.title} | ${config.title}`],
+                ["lang", config.lang],
+                ["description", data.meta.description],
+                ["content", data.content]
+            ].forEach(q => {
+                cache = cache.replace(`%${q[2]}%`, q[1]);
+            })
+
+            fs.mkdirSync(`public/${name}`);
+            // get file name exclude: extension name
+            fs.writeFileSync(`public/${name}/index.html`, cache);
+        })
+
+} catch(e) {
+    console.error(e);
+}
 
 
 
