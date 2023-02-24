5b6e455aaa25a5c8a5b5ffe0e2709acc8761ed5d
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-02-24 17:46:40 +0900

    Fix

--- runner.js
+++ runner.js
@@ -38,6 +38,7 @@ try {
 
             fs.mkdirSync(`public/${name}`);
             // get file name exclude: extension name
+            console.log(cache);
             fs.writeFileSync(`public/${name}/index.html`, cache);
         })
 
